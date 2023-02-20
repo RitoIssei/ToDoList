7d1f72ce4665ab03f7f40155a8b79b018fe5b73a
Tungtt <leouchtk21@gmail.com>
2023-02-20 13:49:51 +0700

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    <TestVue></TestVue>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-import TestVue from "@/components/TestVue.vue"; // @ is an alias to /src
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
 
-@Component({
+export default {
+  name: 'HomeView',
   components: {
-    HelloWorld,
-    TestVue,
-  },
-})
-export default class HomeView extends Vue {}
+    HelloWorld
+  }
+}
 </script>
